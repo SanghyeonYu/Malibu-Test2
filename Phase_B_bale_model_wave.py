067311a559ad6680c518fb0e226943b855700283
SanghyeonYu <yush0123@snu.ac.kr>
2020-09-29 21:54:29 +0900

    20200929 init bale

---
+++ Phase_B_bale_model_wave.py
@@ -0,0 +1,102 @@
+from tensorflow import keras
+from tensorflow.keras.layers import LSTM, Dense, TimeDistributed
+import numpy as np
+import tensorflow as tf
+
+class GatedActivationUnit(keras.layers.Layer):
+    def __init__(self, activation="tanh", **kwargs):
+        super().__init__(**kwargs)
+        self.activation = keras.activations.get(activation)
+    def call(self, inputs):
+        n_filters = inputs.shape[-1] // 2
+        linear_output = self.activation(inputs[..., :n_filters])
+        gate = keras.activations.sigmoid(inputs[..., n_filters:])
+        return self.activation(linear_output) * gate
+
+    def get_config(self):
+        config = super().get_config().copy()
+        config.update({
+            'activation': self.activation,
+        })
+        return config
+
+def wavenet_residual_block(inputs, n_filters, dilation_rate):
+    z = keras.layers.Conv1D(2 * n_filters, kernel_size=2, padding="causal",
+                            dilation_rate=dilation_rate)(inputs)
+    z = GatedActivationUnit()(z)
+    z = keras.layers.Conv1D(n_filters, kernel_size=1)(z)
+    return keras.layers.Add()([z, inputs]), z
+
+keras.backend.clear_session()
+np.random.seed(42)
+tf.random.set_seed(42)
+
+n_dense_layers = 3
+n_layers_per_block = 9 # 10 in the paper
+n_blocks = 3 # 3 in the paper
+n_filters = 128 # 128 in the paper
+n_mc_outputs = 32
+n_outputs = 2 # 256 in the paper
+
+
+
+
+
+
+
+input_sec = keras.layers.Input(shape=(420, 2))
+# print(input_sec)
+z = keras.layers.Conv1D(n_filters, kernel_size=2, padding="causal")(input_sec)
+# print(z)
+z = keras.layers.Conv1D(n_filters, kernel_size=2, padding="causal")(z)
+skip_to_last = []
+for dilation_rate in [2**i for i in range(n_layers_per_block)] * n_blocks:
+    z, skip = wavenet_residual_block(z, n_filters, dilation_rate)
+    skip_to_last.append(skip)
+z = keras.activations.relu(keras.layers.Add()(skip_to_last))
+z = keras.layers.Conv1D(n_filters, kernel_size=1, activation="relu")(z)
+# print(z)
+
+z = keras.layers.Dropout(rate=0.3)(z)
+y_proba = keras.layers.Conv1D(n_outputs, kernel_size=1, activation="sigmoid")(z)
+# print(Y_proba)
+
+model = keras.models.Model(inputs=[input_sec], outputs=[y_proba])
+
+
+# class WAVE_Net(keras.Model):
+#     def __init__(self, **kwargs):
+#         super().__init__(**kwargs)
+#         self.wave_net = model
+#
+#     def call(self, inputs):
+#         return self.wave_net(inputs)
+
+
+
+
+#
+# class WAVE_Net(keras.Model):
+#     def __init__(self, n_units1, n_units2, n_units3, n_units4, layer_num, **kwargs):
+#         super().__init__(**kwargs)
+#         self.hidden1 = LSTM(n_units1, return_sequences=True)
+#         self.hidden2 = LSTM(n_units2, return_sequences=True)
+#         self.hidden3 = LSTM(n_units3, return_sequences=True)
+#         self.hidden4 = LSTM(n_units4, return_sequences=True)
+#         self.out = TimeDistributed(Dense(1))
+#
+#         self.layer_num = layer_num
+#
+#     def call(self, inputs):
+#         hidden1 = self.hidden1(inputs)
+#         if self.layer_num == 2:
+#             return self.out(hidden1)
+#         hidden2 = self.hidden2(hidden1)
+#         if self.layer_num == 3:
+#             return self.out(hidden2)
+#         hidden3 = self.hidden3(hidden2)
+#         if self.layer_num == 4:
+#             return self.out(hidden3)
+#         hidden4 = self.hidden4(hidden3)
+#         if self.layer_num == 5:
+#             return self.out(hidden4)
---
+++ Phase_B_bale_train.py
@@ -0,0 +1,107 @@
+import os
+import numpy as np
+import pandas as pd
+import time
+
+import tensorflow as tf
+# GPU 메모리 증가 허용
+physical_devices = tf.config.list_physical_devices('GPU')
+try:
+    print("memory_growth!!")
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+except:
+    print("!!!!!!!!!")
+  # Invalid device or cannot modify virtual devices once initialized.
+    pass
+
+from tensorflow import keras
+from Phase_B_Data_Loader import *
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.optimizers import Adam, Adadelta, RMSprop
+from tensorflow.keras.models import load_model
+from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard, EarlyStopping
+
+import Phase_B_model_wave as wave_net
+
+
+
+if __name__ == "__main__":
+    # 데이터 로드
+    data_loader = Data_Loader()
+    data_volume_profile, data_sec_data, data_label = data_loader.get_stacked_dataset()
+    # print(data_volume_profile.shape)
+    # print(data_volume_profile[0])
+    # print(data_sec_data.shape)
+    # print(data_sec_data[0])
+    # print(data_label.shape)
+    # print(data_label[0][:50])
+
+    tf.random.set_seed(42)
+    data_volume_profile = tf.random.shuffle(data_volume_profile)
+    tf.random.set_seed(42)
+    data_sec_data = data_sec_data[:, :420, :]
+    data_sec_data = tf.random.shuffle(data_sec_data)
+    tf.random.set_seed(42)
+    data_label = data_label[:, :, :2]
+    data_label = tf.random.shuffle(data_label)
+
+    # sample_num = len(data_label)
+    # test_size = 0.1
+    # train_num = int((1 - test_size) * sample_num)
+
+    # X_train_vol, X_train_sec, y_train = data_volume_profile[:train_num], data_sec_data[:train_num], data_label[:train_num]
+    # X_test_vol, X_test_sec, y_test = data_volume_profile[train_num:], data_sec_data[train_num:], data_label[train_num:]
+
+    X_train = data_sec_data
+    y_train = data_label
+
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
+
+    load = False
+    model_path = "./model_B"
+    summary_path = "./summary_B"
+
+    if not os.path.exists(model_path):
+        os.makedirs(model_path)
+
+    if not os.path.exists(summary_path):
+        os.makedirs(summary_path)
+
+    optimizer_name = "Adam"
+    lr = 0.001
+
+    if load:
+        model = wave_net.model
+        model.load_weights(model_path + "/model.ckpt")
+    else:
+        model = wave_net.model
+
+    if optimizer_name == "Adam":
+        optimizer = Adam(lr=lr)
+    elif optimizer_name == "AdaDelta":
+        optimizer = Adadelta(lr=lr)
+    else:
+        optimizer = RMSprop(lr=lr)
+
+    model.compile(loss=tf.keras.losses.BinaryCrossentropy(),
+                  optimizer=optimizer,
+                  metrics=[tf.keras.metrics.Accuracy(), tf.keras.metrics.Precision(), tf.keras.metrics.Recall()])
+
+
+    checkpoint_cb = ModelCheckpoint(model_path + "/model.ckpt", save_weights_only=True, save_best_only=True)
+    tensorboard_cb = TensorBoard(summary_path)
+    early_stopping_cb = EarlyStopping(patience=1000, restore_best_weights=True)
+
+    history = model.fit(X_train, y_train, validation_split=0.2, epochs=1000000,
+                        callbacks=[checkpoint_cb, tensorboard_cb, early_stopping_cb])
+
+    # loss, accuracy, precision, recall = model.evaluate(X_test, y_test)
+
+    # model_path_tf = os.path.join(os.getcwd(), 'model_B', 'model')
+    # model.save_weights(model_path_tf, save_format="tf")
+
+    # print(X_test_vol.shape)
+    # print(X_test_sec.shape)
+    # print(model.predict((X_test_vol, X_test_sec)).shape)
+    # print(loss, accuracy, precision, recall)
+    model.summary()
